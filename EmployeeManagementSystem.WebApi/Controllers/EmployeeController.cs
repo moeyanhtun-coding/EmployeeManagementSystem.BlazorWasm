ab1e454e2c0d83cf4a820680c0b6d95a52179e13
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-13 13:51:38 +0630

    [Add] EmployeeController and remove WeatherForecastController

---
+++ EmployeeManagementSystem.WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeManagementSystem.BusinessLogic.Services;
+using EmployeeManagementSystem.Model.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            this.employeeService = employeeService;
+        }
+
+        [HttpGet("employeeList")]
+        public async Task<ActionResult<BaseResponseModel>> GetEmployeeList()
+        {
+           var employees =  await employeeService.GetEmployeeListAsync();
+            if(employees is null)
+                return BadRequest(new BaseResponseModel { Data = null, IsSuccess = true,Message = "Employee Not Found"});
+            return Ok(new BaseResponseModel { Data = employees, IsSuccess = true, Message = "Employee List" });
+        }
+    }
+}
