1babcd93c70bd9d7bb825331ce889f5c114eedde
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-13 13:52:29 +0630

    [Refactor] project structure and remove weather component

---
+++ EmployeeManagementSystem.BusinessLogic/Services/EmployeeService.cs
@@ -0,0 +1,31 @@
+﻿using EmployeeManagementSystem.BusinessLogic.Repositories;
+using EmployeeManagementSystem.Database.Data;
+using EmployeeManagementSystem.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.BusinessLogic.Services
+{
+    public interface IEmployeeService
+    {
+        Task<List<EmployeeModel>> GetEmployeeListAsync();
+    }
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeRepository employeeRepository;
+
+        public EmployeeService(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+
+        public async Task<List<EmployeeModel>> GetEmployeeListAsync()
+        {
+            return await employeeRepository.GetEmployeeList();
+        }
+    }
+}
