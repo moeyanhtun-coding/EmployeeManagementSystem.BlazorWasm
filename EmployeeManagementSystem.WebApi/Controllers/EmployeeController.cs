cf49602ae6389bf711faece76f9ead3b03e275b7
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-13 22:02:15 +0630

    [Add] employee creation method and improve formatting

--- EmployeeManagementSystem.WebApi/Controllers/EmployeeController.cs
+++ EmployeeManagementSystem.WebApi/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeManagementSystem.BusinessLogic.Services;
+using EmployeeManagementSystem.Model.Entities;
 using EmployeeManagementSystem.Model.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -19,10 +20,18 @@ namespace EmployeeManagementSystem.WebApi.Controllers
         [HttpGet("employeeList")]
         public async Task<ActionResult<BaseResponseModel>> GetEmployeeList()
         {
-           var employees =  await employeeService.GetEmployeeListAsync();
-            if(employees is null)
-                return BadRequest(new BaseResponseModel { Data = null, IsSuccess = true,Message = "Employee Not Found"});
+            var employees = await employeeService.GetEmployeeListAsync();
+            if (employees is null)
+                return BadRequest(new BaseResponseModel { Data = null, IsSuccess = true, Message = "Employee Not Found" });
             return Ok(new BaseResponseModel { Data = employees, IsSuccess = true, Message = "Employee List" });
+        }
+
+        [HttpPost("createEmployee")]
+        public async Task<ActionResult<BaseResponseModel>> CreateEmployee(EmployeeModel employeeModel)
+        {
+            var res = await employeeService.CreateEmployee(employeeModel);
+            if (res > 0) return Ok(new BaseResponseModel { Data = employeeModel, IsSuccess = true, Message = "Employee Creation Successful" });
+            return BadRequest(new BaseResponseModel { Data = null, IsSuccess = false, Message = "Employee Creation Failed" });
         }
     }
 }
