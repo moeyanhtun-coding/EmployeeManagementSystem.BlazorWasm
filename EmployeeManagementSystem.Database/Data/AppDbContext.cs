93b44ba69cf2670b56e346d346e5e1f0904b37ee
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-13 13:24:49 +0630

    [Add] AppDbContext for Entity Framework integration

---
+++ EmployeeManagementSystem.Database/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeManagementSystem.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Database.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected AppDbContext()
+    {
+    }
+
+    public DbSet<EmployeeModel> Employees { get; set; } = null!;
+}
