dae5fb33a8a2e0f206c06f62d91acaa21ec45a5f
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-14 18:01:48 +0630

    [Add] EmployeeUpdate page for editing employee details

---
+++ EmployeeManagementSystem.Wasm/Pages/Employee/EmployeeUpdate.razor.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeManagementSystem.Model.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagementSystem.Wasm.Pages.Employee
+{
+    public partial class EmployeeUpdate
+    {
+        [Parameter]
+        public int Id { get; set; }
+        private EmployeeModel employee = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            
+        }
+    }
+}
