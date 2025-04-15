f7f2f62f5aef63557a413848cac77fb8a7e8e5b6
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-15 22:04:43 +0630

    [Add] DevCode class for handling authorization headers

---
+++ EmployeeManagementSystem.Wasm/DevCode.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http.Headers;
+
+namespace EmployeeManagementSystem.Wasm
+{
+    public class DevCode
+    {
+       private readonly ILocalStorageService localStorage;
+        private readonly HttpClient httpClient;
+
+        public DevCode(ILocalStorageService localStorage, HttpClient httpClient)
+        {
+            this.localStorage = localStorage;
+            this.httpClient = httpClient;
+        }
+
+        public async Task SetAuthorizeHeader()
+        {
+            var token = (await localStorage.GetItemAsync<string>("authToken")).ToString();
+            if (token is not null)
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+    }
+}
