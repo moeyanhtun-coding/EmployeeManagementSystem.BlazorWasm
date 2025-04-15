6db26378d23996cc85a323d0003b0e44d783681e
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-15 18:36:02 +0630

    [Add] OpenAPI support in ModularServices.cs

--- EmployeeManagementSystem.WebApi/ModularServices.cs
+++ EmployeeManagementSystem.WebApi/ModularServices.cs
@@ -4,6 +4,7 @@ using EmployeeManagementSystem.Database.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 namespace EmployeeManagementSystem.WebApi
