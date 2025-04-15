885899b244a06083fabced30bf6b3b6a0a0db07c
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-15 22:07:08 +0630

    [Add] ValidationErrorResponse class for error handling

---
+++ EmployeeManagementSystem.Model/Models/ValidationErrorResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Model.Models
+{
+    public class ValidationErrorResponse
+    {
+        public string Title { get; set; }
+        public int Status { get; set; }
+        public Dictionary<string, string[]> Errors { get; set; }
+    }
+}
