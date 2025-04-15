8d9870c0105d3447bcf1259751fc896b702951e2
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-15 22:04:28 +0630

    [Add] DevCode service and cascading authentication state

--- EmployeeManagementSystem.Wasm/Program.cs
+++ EmployeeManagementSystem.Wasm/Program.cs
@@ -9,7 +9,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7230") });
+builder.Services.AddScoped<DevCode>();
 builder.Services.AddAuthorizationCore();
+builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 builder.Services.AddBlazoredToast();
 builder.Services.AddBlazoredLocalStorage();
