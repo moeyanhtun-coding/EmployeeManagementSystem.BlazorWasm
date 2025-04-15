215f2cee647b6654b32619fa6b50e71476116abd
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-15 18:31:53 +0630

    [Add] CustomAuthStateProvider for user authentication

---
+++ EmployeeManagementSystem.Wasm/Authentication/CustomAuthStateProvider.cs
@@ -0,0 +1,28 @@
+﻿namespace EmployeeManagementSystem.Wasm.Authentication
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService localStorage;
+
+        public CustomAuthStateProvider(ILocalStorageService localStorage)
+        {
+            this.localStorage = localStorage;
+        }
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task MarkUserAsAuthenticated(string token)
+        {
+           await localStorage.SetItemAsync("authToken", token);
+           var identity = GetClaimsIdentity(token);
+        }
+
+        private ClaimsIdentity GetClaimsIdentity(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+        }
+    }
+}
