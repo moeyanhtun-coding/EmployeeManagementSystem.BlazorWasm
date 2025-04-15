926163d796fa8c582b0367861a225ea7f4e6b097
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-15 12:07:11 +0630

    [Add] LoginResponseModel class for authentication

---
+++ EmployeeManagementSystem.Model/Models/LoginResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Model.Models
+{
+    public class LoginResponseModel
+    {
+        public string Token { get; set; }
+        public long TokenExpired { get; set; } 
+    }
+}
