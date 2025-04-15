a418747f0325444edff0ab6b04a8dbb0b6643142
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-15 18:35:09 +0630

    [Add] global using directives for common namespaces

---
+++ EmployeeManagementSystem.Wasm/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿// Global using directives
+
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using Blazored.LocalStorage;
+global using Microsoft.AspNetCore.Components.Authorization;
