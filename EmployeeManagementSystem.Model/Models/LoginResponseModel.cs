a41ba7153e87fcf2206b1dc452b0ceca5848c0ca
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-16 10:57:27 +0630

    [Add] RefreshToken property to LoginResponseModel

--- EmployeeManagementSystem.Model/Models/LoginResponseModel.cs
+++ EmployeeManagementSystem.Model/Models/LoginResponseModel.cs
@@ -10,5 +10,6 @@ namespace EmployeeManagementSystem.Model.Models
     {
         public string Token { get; set; }
         public long TokenExpired { get; set; } 
+        public string RefreshToken { get; set; }
     }
 }
