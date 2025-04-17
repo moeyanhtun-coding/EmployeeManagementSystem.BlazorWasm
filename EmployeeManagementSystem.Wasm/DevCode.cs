1699c6fcf02344fd545f6d02b1a8422d59a7a13b
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-17 21:51:43 +0630

    [Add] JWT Token Validation for Expire Date and Refresh Token Process

--- EmployeeManagementSystem.Wasm/DevCode.cs
+++ EmployeeManagementSystem.Wasm/DevCode.cs
@@ -1,23 +1,54 @@
-﻿using System.Net.Http.Headers;
+﻿using EmployeeManagementSystem.Model.Models;
+using EmployeeManagementSystem.Wasm.Authentication;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
 
 namespace EmployeeManagementSystem.Wasm
 {
     public class DevCode
     {
-       private readonly ILocalStorageService localStorage;
+        private readonly ILocalStorageService localStorage;
         private readonly HttpClient httpClient;
+        private readonly AuthenticationStateProvider authenticationStateProvider;
+        private readonly NavigationManager nav;
 
-        public DevCode(ILocalStorageService localStorage, HttpClient httpClient)
+        public DevCode(ILocalStorageService localStorage, HttpClient httpClient, AuthenticationStateProvider authenticationStateProvider, NavigationManager nav)
         {
             this.localStorage = localStorage;
             this.httpClient = httpClient;
+            this.authenticationStateProvider = authenticationStateProvider;
+            this.nav = nav;
         }
 
         public async Task SetAuthorizeHeader()
         {
-            var token = (await localStorage.GetItemAsync<string>("authToken")).ToString();
-            if (token is not null)
-                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var sessionState = (await localStorage.GetItemAsync<LoginResponseModel>("sessionState"));
+            if (sessionState is not null && !string.IsNullOrEmpty(sessionState.Token))
+            {
+                if (sessionState.TokenExpired < DateTimeOffset.UtcNow.ToUnixTimeSeconds())
+                {
+                    await ((CustomAuthStateProvider)authenticationStateProvider).MarkUserAsLogout();
+                    nav.NavigateTo("/login");
+                }
+                else if (sessionState.TokenExpired < DateTimeOffset.UtcNow.AddMinutes(5).ToUnixTimeSeconds())
+                {
+                    var res = await httpClient.GetFromJsonAsync<LoginResponseModel>($"/api/auth/loginByRefreshToken?refreshToken={sessionState.RefreshToken}");
+                    if (res is not null)
+                    {
+                        await ((CustomAuthStateProvider)authenticationStateProvider).MarkUserAsAuthenticated(res);
+                        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", res.Token);
+                    }
+                    else
+                    {
+                        await ((CustomAuthStateProvider)authenticationStateProvider).MarkUserAsLogout();
+                    }
+                }
+                else
+                {
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", sessionState.Token);
+                }
+            }
         }
     }
 }
