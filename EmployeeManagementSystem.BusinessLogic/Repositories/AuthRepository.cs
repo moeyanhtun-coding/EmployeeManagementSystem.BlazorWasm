b9a69446076338e1676f34e8dccc10f34a84bc4f
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-18 23:53:13 +0630

    [Add] AddRefreshToken to database and Modify User Register

--- EmployeeManagementSystem.BusinessLogic/Repositories/AuthRepository.cs
+++ EmployeeManagementSystem.BusinessLogic/Repositories/AuthRepository.cs
@@ -9,6 +9,7 @@ namespace EmployeeManagementSystem.BusinessLogic.Repositories
     {
         Task<BaseResponseModel> LoginUserAsync(LoginModel model);
         Task<BaseResponseModel> RegisterUserAsync(RegisterModel model);
+        Task AddRefreshTokenAsync(RefreshTokenModel model);
     }
 
     public class AuthRepository : IAuthRepository
@@ -18,6 +19,22 @@ namespace EmployeeManagementSystem.BusinessLogic.Repositories
         public AuthRepository(AppDbContext context)
         {
             this.context = context;
+        }
+
+        public async Task AddRefreshTokenAsync(RefreshTokenModel model)
+        {
+            var refreshToken = await context.RefreshToken.FirstOrDefaultAsync(x => x.UserId == model.UserId);
+            if (refreshToken is null)
+            {
+                await context.RefreshToken.AddAsync(model);
+                await context.SaveChangesAsync();
+            }
+            else
+            {
+                refreshToken.RefreshToken = model.RefreshToken;
+                context.Entry(refreshToken).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+            }
         }
 
         public async Task<BaseResponseModel> LoginUserAsync(LoginModel model)
@@ -35,8 +52,6 @@ namespace EmployeeManagementSystem.BusinessLogic.Repositories
                 return new BaseResponseModel
                 {
                     IsSuccess = false,
-                    Message = "Credential Do Not Match",
-                    Data = null
                 };
             return new BaseResponseModel
             {
@@ -50,31 +65,66 @@ namespace EmployeeManagementSystem.BusinessLogic.Repositories
         {
             var user = await GetUserByEmail(model.Email!);
             if (user is not null)
+
                 return new BaseResponseModel
                 {
                     IsSuccess = false,
                     Message = "User already exists!",
                     Data = user
                 };
-            var newUser = new UserModel
+
+            using (var transaction = await context.Database.BeginTransactionAsync())
             {
-                UserName = model.Name,
-                Email = model.Email,
-                Password = BCrypt.Net.BCrypt.HashPassword(model.Password)
-            };
-            await context.Users.AddAsync(newUser);
-            await context.SaveChangesAsync();
-            return new BaseResponseModel
-            {
-                IsSuccess = true,
-                Message = "User registered!",
-                Data = newUser
-            };
+                try
+                {
+
+                    #region Add User
+                    var newUser = new UserModel
+                    {
+                        UserName = model.Name,
+                        Email = model.Email,
+                        Password = BCrypt.Net.BCrypt.HashPassword(model.Password)
+                    };
+                    await context.Users.AddAsync(newUser);
+                    await context.SaveChangesAsync();
+                    #endregion
+
+                    #region Add UserRole
+                    var userRole = new UserRoleModel
+                    {
+                        UserId = newUser.UserId,
+                        RoleId = 2
+                    };
+                    context.UserRoles.Add(userRole);
+                    await context.SaveChangesAsync();
+                    #endregion
+
+                    return new BaseResponseModel
+                    {
+                        IsSuccess = true,
+                        Message = "User registered!",
+                        Data = newUser
+                    };
+                }
+                catch (Exception ex)
+                {
+                    await transaction.RollbackAsync();
+
+                    return new BaseResponseModel
+                    {
+                        IsSuccess = false,
+                        Message = $"Registration failed: {ex.Message}",
+                        Data = null
+                    };
+                }
+            }
+
+
         }
 
         private async Task<UserModel> GetUserByEmail(string email)
         {
-            
+
             var user = await context.Users.FirstOrDefaultAsync(x => x.Email == email);
             return user;
         }
