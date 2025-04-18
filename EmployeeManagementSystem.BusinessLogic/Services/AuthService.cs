eabf8c118ffa5988b4a3b7db9331e5404b32afa3
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-18 23:53:18 +0630

    -

---
+++ EmployeeManagementSystem.BusinessLogic/Services/AuthService.cs
@@ -0,0 +1,37 @@
+﻿using EmployeeManagementSystem.BusinessLogic.Repositories;
+using EmployeeManagementSystem.Model.Entities;
+using EmployeeManagementSystem.Model.Models;
+
+namespace EmployeeManagementSystem.BusinessLogic.Services
+{
+    public interface IAuthService
+    {
+        Task<BaseResponseModel> LoginUserAsync(LoginModel model);
+        Task<BaseResponseModel> RegisterUserAsync(RegisterModel model);
+        Task AddRefreshTokenAsync(RefreshTokenModel model);
+    }
+    public class AuthService : IAuthService
+    {
+        private readonly IAuthRepository authRepository;
+
+        public AuthService(IAuthRepository authRepository)
+        {
+            this.authRepository = authRepository;
+        }
+
+        public async Task AddRefreshTokenAsync(RefreshTokenModel model)
+        {
+            await authRepository.AddRefreshTokenAsync(model);
+        }
+
+        public async Task<BaseResponseModel> LoginUserAsync(LoginModel model)
+        {
+            return await authRepository.LoginUserAsync(model);
+        }
+
+        public async Task<BaseResponseModel> RegisterUserAsync(RegisterModel model)
+        {
+            return await authRepository.RegisterUserAsync(model);
+        }
+    }
+}
