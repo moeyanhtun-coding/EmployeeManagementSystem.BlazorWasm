c0699525e16f56618a2c1f6f0d8050b24c03c023
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-18 16:16:25 +0630

    [Add] User Database

---
+++ EmployeeManagementSystem.Model/Entities/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManagementSystem.Model.Entities
+{
+    [Table("Tbl_User")]
+    public class UserModel
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
