635540fc5fc3bb2b349f61b2c767ada6fdedf519
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-18 16:16:06 +0630

    [Add} Role Database

---
+++ EmployeeManagementSystem.Model/Entities/RoleModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManagementSystem.Model.Entities
+{
+    [Table("Tbl_Role")] 
+    public class RoleModel
+    {
+        [Key]
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
