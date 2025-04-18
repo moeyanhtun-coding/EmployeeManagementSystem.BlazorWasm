30739135b511353aea11db9a40dd8be26a8c11a7
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-18 16:16:48 +0630

    [Add] UserRole Database

---
+++ EmployeeManagementSystem.Model/Entities/UserRoleModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManagementSystem.Model.Entities
+{
+    [Table("Tbl_UserRole")]
+    public class UserRoleModel
+    {
+        [Key]
+        public int UserRoleId { get; set; }
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+    }
+}
