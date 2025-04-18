f2f4aabd243b70a813c341ac82c6b23fb3faf7a0
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-18 16:15:42 +0630

    [Add] RefreshToken Table Added

---
+++ EmployeeManagementSystem.Model/Entities/RefreshTokenModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManagementSystem.Model.Entities
+{
+    [Table("Tbl_RefreshToken")]
+    public class RefreshTokenModel
+    {
+        [Key]
+        public int RefreshTokenId { get; set; }
+        public int UserId { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
