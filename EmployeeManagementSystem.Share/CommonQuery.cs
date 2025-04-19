365c55c54855eec968a09d60135d557433ae6bad
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-19 20:06:24 +0630

    [Create] CommonQuery

---
+++ EmployeeManagementSystem.Share/CommonQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Share
+{
+    public class CommonQuery
+    {
+        public static string GetUserList { get; } = "SELECT * FROM Tbl_User";
+    }
+}
