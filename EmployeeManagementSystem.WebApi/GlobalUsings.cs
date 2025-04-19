b1f121572d15a0066bf48b90d522622fd6e6fb90
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-19 20:37:47 +0630

    -

---
+++ EmployeeManagementSystem.WebApi/GlobalUsings.cs
@@ -0,0 +1,18 @@
+// Global using directives
+
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using System.Text;
+global using EmployeeManagementSystem.BusinessLogic.Repositories;
+global using EmployeeManagementSystem.BusinessLogic.Services;
+global using EmployeeManagementSystem.Database.Data;
+global using EmployeeManagementSystem.Model.Entities;
+global using EmployeeManagementSystem.Model.Models;
+global using EmployeeManagementSystem.Share;
+global using EmployeeManagementSystem.WebApi;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.OpenApi.Models;
