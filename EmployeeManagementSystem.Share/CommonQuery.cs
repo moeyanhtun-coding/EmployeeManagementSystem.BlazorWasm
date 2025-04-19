52133d7c495d5ed597b018e7d8590410415ca12d
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-19 20:33:51 +0630

    [Refector] Global Using

--- EmployeeManagementSystem.Share/CommonQuery.cs
+++ EmployeeManagementSystem.Share/CommonQuery.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EmployeeManagementSystem.Share
+﻿namespace EmployeeManagementSystem.Share
 {
     public class CommonQuery
     {
