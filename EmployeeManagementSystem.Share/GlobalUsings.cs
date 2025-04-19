48c38cf47696c5e3c0bb13d68dab46fc070e151c
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-19 20:37:38 +0630

    -

---
+++ EmployeeManagementSystem.Share/GlobalUsings.cs
@@ -0,0 +1,10 @@
+// Global using directives
+
+global using System;
+global using System.Collections.Generic;
+global using System.Data;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using Dapper;
+global using Microsoft.Data.SqlClient;
