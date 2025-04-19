0e349300f81a2fa2d4b7fffeda074284ffe8cc20
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-20 00:28:36 +0630

    [Add] GetUserDetail Query

--- EmployeeManagementSystem.Share/CommonQuery.cs
+++ EmployeeManagementSystem.Share/CommonQuery.cs
@@ -2,6 +2,10 @@
 {
     public class CommonQuery
     {
-        public static string GetUserList { get; } = "SELECT * FROM Tbl_User";
+        public static string GetUserDetail { get; } = @"Select Tbl_User.*, Tbl_Role.RoleName 
+                                                        From Tbl_User 
+                                                        Join Tbl_UserRole on  Tbl_UserRole.UserId = Tbl_User.UserId
+                                                        Join Tbl_Role on Tbl_Role.RoleId = Tbl_UserRole.RoleId 
+                                                        Where Tbl_User.UserId = @UserId";
     }
 }
