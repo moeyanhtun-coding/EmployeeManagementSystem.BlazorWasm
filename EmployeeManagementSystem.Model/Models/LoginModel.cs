a83a5171563ef4ae6ebeed6e76ff247ee93c09c8
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-19 20:33:33 +0630

    [Refector] Global Using

--- EmployeeManagementSystem.Model/Models/LoginModel.cs
+++ EmployeeManagementSystem.Model/Models/LoginModel.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EmployeeManagementSystem.Model.Models
+﻿namespace EmployeeManagementSystem.Model.Models
 {
     public class LoginModel
     {
