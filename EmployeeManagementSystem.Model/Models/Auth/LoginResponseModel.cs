1a7e2c12ea1af51b5f82692418ab7592a73f7129
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-20 00:27:41 +0630

    -

---
+++ EmployeeManagementSystem.Model/Models/Auth/LoginResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeManagementSystem.Model.Models.Auth
+{
+    public class LoginResponseModel
+    {
+        public string Token { get; set; }
+        public long TokenExpired { get; set; } 
+        public string RefreshToken { get; set; }
+    }
+}
