22495ca76f0982c6777d4d3651f746f32d941355
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-20 00:27:45 +0630

    -

---
+++ EmployeeManagementSystem.Model/Models/Auth/RegisterModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeManagementSystem.Model.Models.Auth;
+
+public class RegisterModel
+{
+    [Required] public string? Name { get; set; }
+    [Required, EmailAddress] public string? Email { get; set; }
+
+    [Required, StringLength(100, MinimumLength = 6)]
+    public string? Password { get; set; }
+
+    [Required, Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string? ConfirmPassword { get; set; }
+}
