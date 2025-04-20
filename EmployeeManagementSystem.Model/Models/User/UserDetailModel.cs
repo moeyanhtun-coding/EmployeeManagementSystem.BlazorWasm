e8ec409dbb63c89a66b4da8ed9c01e817473ac69
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-20 19:20:54 +0630

    -

--- EmployeeManagementSystem.Model/Models/User/UserDetailModel.cs
+++ EmployeeManagementSystem.Model/Models/User/UserDetailModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EmployeeManagementSystem.Model.Models.User
+﻿namespace EmployeeManagementSystem.Model.Models.User
 {
     public class UserDetailModel
     {
