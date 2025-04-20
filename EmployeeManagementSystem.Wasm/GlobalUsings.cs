125c916ff911b9c928c39a59f4f4cacffa2741d7
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-20 19:20:40 +0630

    -

--- EmployeeManagementSystem.Wasm/GlobalUsings.cs
+++ EmployeeManagementSystem.Wasm/GlobalUsings.cs
@@ -1,15 +1,21 @@
 ﻿// Global using directives
 
+global using System.Dynamic;
 global using System.IdentityModel.Tokens.Jwt;
 global using System.Net.Http.Headers;
 global using System.Net.Http.Json;
 global using System.Security.Claims;
 global using Blazored.LocalStorage;
 global using Blazored.Toast;
+global using EmployeeManagementSystem.Model.Entities;
 global using EmployeeManagementSystem.Model.Models;
+global using EmployeeManagementSystem.Model.Models.Auth;
 global using EmployeeManagementSystem.Wasm;
 global using EmployeeManagementSystem.Wasm.Authentication;
+global using EmployeeManagementSystem.Wasm.Compoments.BaseCompoments;
 global using Microsoft.AspNetCore.Components;
 global using Microsoft.AspNetCore.Components.Authorization;
 global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using Microsoft.EntityFrameworkCore.Storage.Json;
+global using Newtonsoft.Json;
