d716e3360620c0489f59fd88664b8dc0112d4333
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-20 19:12:53 +0630

    [Add] GetUserDetailByRefreshToken Query

--- EmployeeManagementSystem.Share/CommonQuery.cs
+++ EmployeeManagementSystem.Share/CommonQuery.cs
@@ -2,10 +2,19 @@
 {
     public class CommonQuery
     {
-        public static string GetUserDetail { get; } = @"Select Tbl_User.*, Tbl_Role.RoleName 
-                                                        From Tbl_User 
-                                                        Join Tbl_UserRole on  Tbl_UserRole.UserId = Tbl_User.UserId
-                                                        Join Tbl_Role on Tbl_Role.RoleId = Tbl_UserRole.RoleId 
-                                                        Where Tbl_User.UserId = @UserId";
+        public static string GetUserDetail { get; } =
+            @"SELECT Tbl_User.*, Tbl_Role.RoleName 
+            FROM Tbl_User 
+            JOIN Tbl_UserRole ON  Tbl_UserRole.UserId = Tbl_User.UserId
+            JOIN Tbl_Role ON Tbl_Role.RoleId = Tbl_UserRole.RoleId 
+            WHERE Tbl_User.UserId = @UserId";
+
+        public static string GetUserDetailByRefreshTokenId { get; } =
+            @"SELECT  Tbl_User.*, Tbl_Role.RoleName
+            FROM Tbl_RefreshToken
+            JOIN Tbl_User ON Tbl_RefreshToken.UserId = Tbl_User.UserId
+            JOIN Tbl_UserRole ON Tbl_User.UserId = Tbl_UserRole.UserId
+            JOIN Tbl_Role ON Tbl_UserRole.RoleId = Tbl_Role.RoleId 
+            WHERE Tbl_RefreshToken.RefreshTokenId = @RefreshTokenId";
     }
 }
