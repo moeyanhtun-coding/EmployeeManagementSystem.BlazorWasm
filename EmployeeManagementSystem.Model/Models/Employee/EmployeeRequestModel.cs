df63e35c7766e9c951035182d0102ce8295810bb
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-21 23:13:03 +0630

    [Create] Employee Response Model

---
+++ EmployeeManagementSystem.Model/Models/Employee/EmployeeRequestModel.cs
@@ -0,0 +1,23 @@
+namespace EmployeeManagementSystem.Model.Models.Employee;
+
+public class EmployeeRequestModel
+{
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    [Required, EmailAddress]
+    public string Email { get; set; }
+    [Required, Phone]
+    public string PhoneNumber { get; set; }
+    [Required, MinLength(6), MaxLength(30)]
+    public string Address { get; set; }
+    [Required]
+    public string DepartmentCode { get; set; }
+    [Required]
+    public string PositionCode { get; set; }
+    [Required]
+    public DateTime DateOfBirth { get; set; }
+    [Required]
+    public DateTime DateOfJoining { get; set; }
+}
