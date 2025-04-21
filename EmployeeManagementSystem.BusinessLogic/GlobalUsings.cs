6a7876ec98227fcbd376a56dc88a80aa3e0b7dae
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-21 19:27:07 +0630

    -

--- EmployeeManagementSystem.BusinessLogic/GlobalUsings.cs
+++ EmployeeManagementSystem.BusinessLogic/GlobalUsings.cs
@@ -1,12 +1,7 @@
 // Global using directives
 
-global using System;
 global using System.Collections.Generic;
-global using System.Data;
-global using System.Linq;
-global using System.Text;
 global using System.Threading.Tasks;
-global using Dapper;
 global using EmployeeManagementSystem.BusinessLogic.Repositories;
 global using EmployeeManagementSystem.Database.Data;
 global using EmployeeManagementSystem.Model.Entities;
