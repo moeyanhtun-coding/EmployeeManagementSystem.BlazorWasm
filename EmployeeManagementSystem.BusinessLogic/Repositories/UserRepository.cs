c79fb1ce329410a96ade42be6cf02c9c04c4b989
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-25 23:08:44 +0630

    [Add] GetUserList User Repository

---
+++ EmployeeManagementSystem.BusinessLogic/Repositories/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeManagementSystem.Model.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.BusinessLogic.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<List<UserDetailModel>> GetUserList();
+    }
+    public class UserRepository : IUserRepository
+    {
+        private readonly IDapperService db;
+
+        public UserRepository(IDapperService db)
+        {
+            this.db = db;
+        }
+
+        public async Task<List<UserDetailModel>> GetUserList()
+        {
+            var lst = await db.QueryAsync<UserDetailModel>(CommonQuery.GetUserDetailList);
+            return lst;
+        }
+    }
+}
