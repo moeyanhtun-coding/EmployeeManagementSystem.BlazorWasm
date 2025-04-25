630bbf276dc37f30369283574a6c93db0250f3c4
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-25 23:10:04 +0630

    [Add] User List Controller

---
+++ EmployeeManagementSystem.WebApi/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.WebApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        [HttpGet("getUserList")]
+        public async Task<ActionResult<BaseResponseModel>> GetUserList()
+        {
+            try
+            {
+                var lst = await userRepository.GetUserList();
+                return Ok(new BaseResponseModel
+                {
+                    IsSuccess = true,
+                    Message = "User List",
+                    Data = lst
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
