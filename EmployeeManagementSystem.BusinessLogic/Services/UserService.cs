9dfd36a55a823ce978ed78958fc9334a6279a084
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-25 23:08:56 +0630

    [Add] GetUserList User Service

---
+++ EmployeeManagementSystem.BusinessLogic/Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeManagementSystem.Model.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.BusinessLogic.Services
+{
+    public interface IUserService
+    {
+        Task<List<UserDetailModel>> GetUserList();
+    }
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public async Task<List<UserDetailModel>> GetUserList()
+        {
+            return await userRepository.GetUserList();
+        }
+    }
+}
