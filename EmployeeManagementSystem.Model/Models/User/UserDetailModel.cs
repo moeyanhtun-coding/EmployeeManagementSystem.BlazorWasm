049d03439e4643f3e8bde63546370f391e03d45e
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-26 16:14:53 +0630

    [Add] More Row

--- EmployeeManagementSystem.Model/Models/User/UserDetailModel.cs
+++ EmployeeManagementSystem.Model/Models/User/UserDetailModel.cs
@@ -3,9 +3,11 @@
     public class UserDetailModel
     {
         public int UserId { get; set; }
+        public string UserCode { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
         public string RoleName { get; set; }
     }
 }
