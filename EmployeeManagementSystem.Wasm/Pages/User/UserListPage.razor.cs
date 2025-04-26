0883212149fda208e20990ac35297e4f41a7700c
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-26 15:12:53 +0630

    -

---
+++ EmployeeManagementSystem.Wasm/Pages/User/UserListPage.razor.cs
@@ -0,0 +1,60 @@
+﻿using EmployeeManagementSystem.Model.Models.User;
+
+namespace EmployeeManagementSystem.Wasm.Pages.User
+{
+    public partial class UserListPage
+    {
+        private List<UserDetailModel> UserDetailModels;
+        private List<UserDetailModel> UserLists;
+        private BaseResponseModel baseResponseModel;
+        private string role;
+        private AppModal Modal;
+        [Inject] private DevCode devCode { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetUserList();
+        }
+        public void GetUserListByRole(string roleName)
+        {
+            role = roleName;
+            if (roleName == "All")
+            {
+                UserLists = UserDetailModels;
+            }
+            else
+            {
+                UserLists = UserDetailModels.Where(x => x.RoleName == roleName).ToList();
+            }
+
+        }
+        public async Task GetUserList()
+        {
+            try
+            {
+                await devCode.SetAuthorizeHeader();
+                var response = await httpClient.GetAsync("api/User/getUserList");
+                if (response.IsSuccessStatusCode)
+                {
+                    var JsonStr = await response.Content.ReadAsStringAsync();
+                    baseResponseModel = JsonConvert.DeserializeObject<BaseResponseModel>(JsonStr)!;
+                    if (baseResponseModel.IsSuccess)
+                    {
+                        UserDetailModels = JsonConvert.DeserializeObject<List<UserDetailModel>>(baseResponseModel.Data!.ToString()!)!;
+                        GetUserListByRole("All");
+                    }
+                }
+                else
+                {
+                    var error = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(error);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
+
