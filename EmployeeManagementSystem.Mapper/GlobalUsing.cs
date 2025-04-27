b0defba346834e8b6ed022846e93832dc8802200
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-27 23:10:19 +0630

    -

---
+++ EmployeeManagementSystem.Mapper/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using BCrypt.Net;
+global using EmployeeManagementSystem.Model.Entities;
+global using EmployeeManagementSystem.Model.Models.Auth;
+global using EmployeeManagementSystem.Model.Models.Employee;
+global using Microsoft.Identity.Client;
