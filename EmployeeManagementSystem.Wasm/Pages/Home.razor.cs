f4631ba1713e620d7352cc5a659fe39e4c9766af
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-28 15:13:29 +0630

    [Add] Get Data

---
+++ EmployeeManagementSystem.Wasm/Pages/Home.razor.cs
@@ -0,0 +1,59 @@
+﻿using EmployeeManagementSystem.Model.Entities;
+using EmployeeManagementSystem.Model.Models;
+using EmployeeManagementSystem.Model.Models.User;
+
+namespace EmployeeManagementSystem.Wasm.Pages
+{
+    public partial class Home
+    {
+        [Inject]
+        private DevCode devCode { get; set; }
+        private List<EmployeeModel> employees { get; set; }
+        private List<UserDetailModel> users { get; set; }
+        protected async override Task OnInitializedAsync()
+        {
+            await GetEmployeeData();
+            await GetUserData();
+        }
+
+        private async Task GetEmployeeData()
+        {
+            await devCode.SetAuthorizeHeader();
+            var response = await httpClient.GetAsync("api/Employee/employeeList");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var baseResponseModel = JsonConvert.DeserializeObject<BaseResponseModel>(jsonResponse)!;
+                if (baseResponseModel!.IsSuccess)
+                {
+                    employees = JsonConvert.DeserializeObject<List<EmployeeModel>>(baseResponseModel.Data!.ToString()!)!;
+                }
+            }
+            else
+            {
+                var error = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Error occurred: " + error.ToString());
+            }
+        }
+
+        private async Task GetUserData()
+        {
+            await devCode.SetAuthorizeHeader();
+            var response = await httpClient.GetAsync("api/User/getUserList");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var baseResponseModel = JsonConvert.DeserializeObject<BaseResponseModel>(jsonResponse);
+                if (baseResponseModel.IsSuccess)
+                {
+                    users = JsonConvert.DeserializeObject<List<UserDetailModel>>(baseResponseModel.Data.ToString());
+                }
+            }
+            else
+            {
+                var error = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Error occurred: " + error.ToString());
+            }
+        }
+    }
+}
