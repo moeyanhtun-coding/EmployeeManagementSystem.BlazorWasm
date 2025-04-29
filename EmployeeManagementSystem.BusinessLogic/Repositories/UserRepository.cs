b7874f06bb58d6807710b299fb57855ecbfa5f02
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-29 22:41:26 +0630

    [Add] User Repository For Get User By Code

--- EmployeeManagementSystem.BusinessLogic/Repositories/UserRepository.cs
+++ EmployeeManagementSystem.BusinessLogic/Repositories/UserRepository.cs
@@ -3,20 +3,28 @@
     public interface IUserRepository
     {
         Task<List<UserDetailModel>> GetUserList();
+        Task<UserDetailByCodeModel> GetUserDetailByCode(string userCode);
     }
     public class UserRepository : IUserRepository
     {
         private readonly IDapperService db;
+        private readonly AppDbContext context;
 
-        public UserRepository(IDapperService db)
+        public UserRepository(IDapperService db, AppDbContext context)
         {
             this.db = db;
+            this.context = context;
         }
 
         public async Task<List<UserDetailModel>> GetUserList()
         {
             var lst = await db.QueryAsync<UserDetailModel>(CommonQuery.GetUserDetailList);
             return lst;
+        }
+
+        public async Task<UserDetailByCodeModel> GetUserDetailByCode(string userCode)
+        {
+            return  await db.QueryFirstOrDefaultAsync<UserDetailByCodeModel>(CommonQuery.GetUserDetailByCode, new { UserCode = userCode });
         }
     }
 }
