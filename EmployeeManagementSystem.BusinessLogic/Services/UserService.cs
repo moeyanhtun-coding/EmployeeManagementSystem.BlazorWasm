970b915e5942ab2665001c6b2a6188c317ea0c2c
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-29 22:41:35 +0630

    [Add] User Service For Get User By Code

--- EmployeeManagementSystem.BusinessLogic/Services/UserService.cs
+++ EmployeeManagementSystem.BusinessLogic/Services/UserService.cs
@@ -3,6 +3,7 @@
     public interface IUserService
     {
         Task<List<UserDetailModel>> GetUserList();
+        Task<UserDetailByCodeModel> GetUserDetailByCode(string userCode);
     }
     public class UserService : IUserService
     {
@@ -11,6 +12,11 @@
         public UserService(IUserRepository userRepository)
         {
             this.userRepository = userRepository;
+        }
+
+        public async Task<UserDetailByCodeModel> GetUserDetailByCode(string userCode)
+        {
+           return await userRepository.GetUserDetailByCode(userCode);
         }
 
         public async Task<List<UserDetailModel>> GetUserList()
