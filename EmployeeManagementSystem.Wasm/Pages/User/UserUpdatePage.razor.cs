6fef96eefac1a4a1f6e4ae68c96f96bf811203b2
Moe Yan Htun <moeyan227970@gmail.com>
2025-04-29 15:35:20 +0630

    [Add partial Class for User Update

---
+++ EmployeeManagementSystem.Wasm/Pages/User/UserUpdatePage.razor.cs
@@ -0,0 +1,38 @@
+﻿using EmployeeManagementSystem.Model.Models.User;
+
+namespace EmployeeManagementSystem.Wasm.Pages.User
+{
+    public partial class UserUpdatePage
+    {
+        [Parameter]
+        public int Id { get; set; }
+        private UserDetailModel userDetailModel = new();
+
+        [Inject]
+        private DevCode devCode { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+
+        }
+
+        public async Task GetUserData()
+        {
+            await devCode.SetAuthorizeHeader();
+            var response = await httpClient.GetAsync($"api/User/getUser/{Id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<BaseResponseModel>(result)!;
+                if (data.IsSuccess)
+                {
+                    userDetailModel = JsonConvert.DeserializeObject<UserDetailModel>(data.Data!.ToString()!)!;
+                }
+            }
+            else
+            {
+                Console.WriteLine(response.ToString());
+            }
+        }
+    }
+}
