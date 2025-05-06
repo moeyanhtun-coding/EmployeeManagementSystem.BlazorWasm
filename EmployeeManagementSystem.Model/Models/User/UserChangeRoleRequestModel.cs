604a37e2c7d9ed99f3121e525d6b40bf38c43bad
Moe Yan Htun <moeyan227970@gmail.com>
2025-05-06 22:49:43 +0630

    [Add] User Change Role Request Model

---
+++ EmployeeManagementSystem.Model/Models/User/UserChangeRoleRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Model.Models.User
+{
+    public  class UserChangeRoleRequestModel
+    {
+        public int RoleId { get; set; }
+    }
+}
