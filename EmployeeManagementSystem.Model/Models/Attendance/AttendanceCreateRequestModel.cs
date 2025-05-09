39998bdc0bd3add2a3293e4eeaefd2d313843a6b
Moe Yan Htun <moeyan227970@gmail.com>
2025-05-09 13:51:25 +0630

    [Create] Attendance Request Model

---
+++ EmployeeManagementSystem.Model/Models/Attendance/AttendanceCreateRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Model.Models.Attendance
+{
+    public class AttendanceCreateRequestModel
+    {
+        public string EmployeeCode { get; set; }
+    }
+}
