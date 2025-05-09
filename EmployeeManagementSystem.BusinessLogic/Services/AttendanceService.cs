685f294e177c0de71205683268c295321df2b684
Moe Yan Htun <moeyan227970@gmail.com>
2025-05-09 21:06:54 +0630

    -

---
+++ EmployeeManagementSystem.BusinessLogic/Services/AttendanceService.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeManagementSystem.Model.Models.Attendance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.BusinessLogic.Services
+{
+    public interface IAttendanceService
+    {
+        Task<BaseResponseModel> AttendanceCreate(AttendanceCreateRequestModel reqModel);
+        Task<BaseResponseModel> AttendanceList();
+
+    }
+
+    public class AttendanceService : IAttendanceService
+    {
+        private readonly IAttendanceRepository attendanceRepository;
+
+        public AttendanceService(IAttendanceRepository attendanceRepository)
+        {
+            this.attendanceRepository = attendanceRepository;
+        }
+
+        public async Task<BaseResponseModel> AttendanceCreate(AttendanceCreateRequestModel reqModel)
+        {
+            return await attendanceRepository.AttendanceCreate(reqModel);
+        }
+
+        //public async Task<BaseResponseModel> AttendanceList()
+        //{
+        //    return await attendanceRepository.AttendanceList();
+        //}
+    }
+}
