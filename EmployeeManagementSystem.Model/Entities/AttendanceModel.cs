d6fe46bbcbe31d766d8722d47118350fdfb2699a
Moe Yan Htun <moeyan227970@gmail.com>
2025-05-09 12:53:29 +0630

    [Create] Attendance Model

---
+++ EmployeeManagementSystem.Model/Entities/AttendanceModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Model.Entities
+{
+    [Table("Tbl_AttendanceLog")]
+    public class AttendanceModel
+    {
+        [Key]
+        public int AttendanceId { get; set; }
+        public string AttendanceCode { get; set; }
+        public string EmployeeCode { get; set; }
+        public DateTime LogTime { get; set; }
+        public string Type { get; set; }
+    }
+}
