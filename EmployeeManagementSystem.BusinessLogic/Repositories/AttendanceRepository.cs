1eb4a7365c696b3952c4dff4de2b1af8c9f47ad0
Moe Yan Htun <moeyan227970@gmail.com>
2025-05-09 13:51:10 +0630

    [Create] Attendance Repository

---
+++ EmployeeManagementSystem.BusinessLogic/Repositories/AttendanceRepository.cs
@@ -0,0 +1,32 @@
+﻿using EmployeeManagementSystem.Model.Models.Attendance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.BusinessLogic.Repositories
+{
+    public interface IAttendanceRepository
+    {
+        Task<int> AttendanceCreate(AttendanceCreateRequestModel reqModel);
+    }
+
+    public class AttendanceRepository : IAttendanceRepository
+    {
+        private readonly AppDbContext _context;
+
+        public AttendanceRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> AttendanceCreate(AttendanceCreateRequestModel reqModel)
+        {
+            var employee = await _context.Employees.FirstOrDefaultAsync(x => x.EmployeeCode == reqModel.EmployeeCode);
+            if (employee is null) 
+            { 
+            }
+        }
+    }
+}
