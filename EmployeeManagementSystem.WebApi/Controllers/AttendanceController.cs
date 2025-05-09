2ff60e37dbaa8fc87b0ce5456129a1c8dd82f591
Moe Yan Htun <moeyan227970@gmail.com>
2025-05-09 13:51:46 +0630

    [Create] Attendance Controller

---
+++ EmployeeManagementSystem.WebApi/Controllers/AttendanceController.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeManagementSystem.BusinessLogic.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttendanceController : ControllerBase
+    {
+        [HttpPost("attendanceEmployee/{employeeCode}")]
+        public async Task<ActionResult<BaseResponseModel>> AttendanceEmployee(string employeeCode)
+        {
+            var res = await employeeService.AttendanceEmployee(employeeCode);
+        }
+    }
+}
