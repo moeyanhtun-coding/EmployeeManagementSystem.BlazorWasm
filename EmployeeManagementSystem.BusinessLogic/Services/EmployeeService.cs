0c215ab83a999a34212dd90ea889d61b375fa842
moeyanhtun-coding <moeyan227970@gmail.com>
2025-05-15 19:22:26 +0630

    [Refactor] Employee Service

--- EmployeeManagementSystem.BusinessLogic/Services/EmployeeService.cs
+++ EmployeeManagementSystem.BusinessLogic/Services/EmployeeService.cs
@@ -1,44 +1,56 @@
-﻿namespace EmployeeManagementSystem.BusinessLogic.Services
+﻿namespace EmployeeManagementSystem.BusinessLogic.Services;
+
+public interface IEmployeeService
 {
-    public interface IEmployeeService
+    Task<List<EmployeeModel>> GetEmployeeListAsync();
+    Task<int> CreateEmployee(EmployeeRequestModel employeeModel);
+    Task<EmployeeModel> GetEmployeeById(int id);
+    Task<int> UpdateEmployee(int id, EmployeeModel employeeModel);
+    Task<int> DeleteEmployee(int id);
+}
+
+public class EmployeeService : IEmployeeService
+{
+    private readonly IEmployeeRepository employeeRepository;
+
+    public EmployeeService(IEmployeeRepository employeeRepository)
     {
-        Task<List<EmployeeModel>> GetEmployeeListAsync();
-        Task<int> CreateEmployee(EmployeeRequestModel employeeModel);
-        Task<EmployeeModel> GetEmployeeById(int id);
-        Task<int> UpdateEmployee(int id, EmployeeModel employeeModel);
-        Task<int> DeleteEmployee(int id);
+        this.employeeRepository = employeeRepository;
     }
-    public class EmployeeService : IEmployeeService
+
+    public async Task<List<EmployeeModel>> GetEmployeeListAsync()
     {
-        private readonly IEmployeeRepository employeeRepository;
-        public EmployeeService(IEmployeeRepository employeeRepository)
-        {
-            this.employeeRepository = employeeRepository;
-        }
+        return await employeeRepository.GetEmployeeList();
+    }
 
-        public async Task<List<EmployeeModel>> GetEmployeeListAsync()
-        {
-            return await employeeRepository.GetEmployeeList();
-        }
+    public async Task<EmployeeModel> GetEmployeeById(int id)
+    {
+        return await employeeRepository.GetEmployeeById(id);
+    }
 
-        public async Task<EmployeeModel> GetEmployeeById(int id)
-        {
-            return await employeeRepository.GetEmployeeById(id);
-        }
+    public async Task<int> CreateEmployee(EmployeeRequestModel employeeModel)
+    {
+        return await employeeRepository.CreateEmployee(employeeModel);
+    }
 
-        public async Task<int> CreateEmployee(EmployeeRequestModel employeeModel)
-        {
-            return await employeeRepository.CreateEmployee(employeeModel);
-        }
+    public async Task<int> UpdateEmployee(int id, EmployeeModel employeeModel)
+    {
+        var employee = await employeeRepository.GetEmployeeById(id);
+        if (employee is null) return 0;
+        employee.FirstName = employeeModel.FirstName;
+        employee.LastName = employeeModel.LastName;
+        employee.Email = employeeModel.Email;
+        employee.PhoneNumber = employeeModel.PhoneNumber;
+        employee.Address = employeeModel.Address;
+        employee.DepartmentCode = employeeModel.DepartmentCode;
+        employee.PositionCode = employeeModel.PositionCode;
+        employee.UpdatedAt = DateTime.Now;
+        return await employeeRepository.UpdateEmployee(employee);
+    }
 
-        public async Task<int> UpdateEmployee(int id, EmployeeModel employeeModel)
-        {
-            return await employeeRepository.UpdateEmployee(id, employeeModel);
-        }
-
-        public async Task<int> DeleteEmployee(int id)
-        {
-            return await employeeRepository.DeleteEmployee(id);
-        }
+    public async Task<int> DeleteEmployee(int id)
+    {
+        var employee = await employeeRepository.GetEmployeeById(id);
+        return await employeeRepository.DeleteEmployee(employee);
     }
 }
