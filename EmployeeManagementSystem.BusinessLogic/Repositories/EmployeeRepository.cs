8c0a2277fbf485e05d44927d5ce954cd3c757518
moeyanhtun-coding <moeyan227970@gmail.com>
2025-05-15 19:22:11 +0630

    [Refactor] EmployeeRepository

--- EmployeeManagementSystem.BusinessLogic/Repositories/EmployeeRepository.cs
+++ EmployeeManagementSystem.BusinessLogic/Repositories/EmployeeRepository.cs
@@ -1,65 +1,48 @@
-﻿namespace EmployeeManagementSystem.BusinessLogic.Repositories
+﻿namespace EmployeeManagementSystem.BusinessLogic.Repositories;
+
+public interface IEmployeeRepository
 {
-    public interface IEmployeeRepository
+    Task<List<EmployeeModel>> GetEmployeeList();
+    Task<EmployeeModel> GetEmployeeById(int id);
+    Task<int> CreateEmployee(EmployeeRequestModel employeeModel);
+    Task<int> UpdateEmployee(EmployeeModel employeeModel);
+    Task<int> DeleteEmployee(EmployeeModel employeeModel);
+}
+
+public class EmployeeRepository : IEmployeeRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public EmployeeRepository(AppDbContext dbContext)
     {
-        Task<List<EmployeeModel>> GetEmployeeList();
-        Task<int> CreateEmployee(EmployeeRequestModel employeeModel);
-        Task<EmployeeModel> GetEmployeeById(int id);
-        Task<int> UpdateEmployee(int id, EmployeeModel employeeModel);
-        Task<int> DeleteEmployee(int id);
+        _dbContext = dbContext;
     }
-    public class EmployeeRepository(AppDbContext _context) : IEmployeeRepository
+
+    public async Task<List<EmployeeModel>> GetEmployeeList()
     {
-        public async Task<List<EmployeeModel>> GetEmployeeList()
-        {
-            return await _context.Employees.ToListAsync();
-        }
-        public async Task<int> CreateEmployee(EmployeeRequestModel employeeModel)
-        {
-            await _context.Employees.AddAsync(employeeModel.Change());
-            return await _context.SaveChangesAsync();
-        }
+        return await _dbContext.Employees.ToListAsync();
+    }
 
-        public async Task<EmployeeModel> GetEmployeeById(int id)
-        {
-            var emoloyee = await _context.Employees.AsNoTracking().FirstOrDefaultAsync(x => x.EmployeeId == id);
-            if (emoloyee is null)
-                return null;
-            return emoloyee;
-        }
+    public async Task<int> CreateEmployee(EmployeeRequestModel employeeModel)
+    {
+        await _dbContext.Employees.AddAsync(employeeModel.Change());
+        return await _dbContext.SaveChangesAsync();
+    }
 
-        public async Task<int> UpdateEmployee(int id, EmployeeModel employeeModel)
-        {
-            var employee = await GetEmployeeById(id);
+    public async Task<EmployeeModel> GetEmployeeById(int id)
+    {
+        return await _dbContext.Employees.AsNoTracking().FirstOrDefaultAsync(x => x.EmployeeId == id);
+    }
 
-            employee.EmployeeCode = employeeModel.EmployeeCode;
-            employee.FirstName = employeeModel.FirstName;
-            employee.LastName = employeeModel.LastName;
-            employee.Email = employeeModel.Email;
-            employee.Address = employeeModel.Address;
-            employee.PhoneNumber = employeeModel.PhoneNumber;
-            employee.DepartmentCode = employeeModel.DepartmentCode;
-            employee.PositionCode = employeeModel.PositionCode;
-            employee.DateOfBirth = employeeModel.DateOfBirth;
-            employee.DateOfJoining = employeeModel.DateOfJoining;
-            employee.UpdatedAt = DateTime.Now;
+    public async Task<int> UpdateEmployee(EmployeeModel employeeModel)
+    {
+        _dbContext.Entry(employeeModel).State = EntityState.Modified;
+        return await _dbContext.SaveChangesAsync();
+    }
 
-            _context.Entry(employee).State = EntityState.Modified;
-            return await _context.SaveChangesAsync();
-        }
-
-        public async Task<int> DeleteEmployee(int id)
-        {
-            var employee = await GetEmployeeById(id);
-            if (employee is null)
-            {
-                return 0;
-            }
-            else
-            {
-                _context.Entry(employee).State = EntityState.Deleted;
-                return await _context.SaveChangesAsync();
-            }
-        }
+    public async Task<int> DeleteEmployee(EmployeeModel employeeModel)
+    {
+        _dbContext.Entry(employeeModel).State = EntityState.Deleted;
+        return await _dbContext.SaveChangesAsync();
     }
 }
