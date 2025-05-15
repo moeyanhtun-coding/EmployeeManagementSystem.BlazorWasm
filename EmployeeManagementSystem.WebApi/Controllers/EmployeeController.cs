6262e92dec33412f3c4f86d738d5678cfd2ca423
moeyanhtun-coding <moeyan227970@gmail.com>
2025-05-15 19:21:55 +0630

    [Refactor] Employee Controller

--- EmployeeManagementSystem.WebApi/Controllers/EmployeeController.cs
+++ EmployeeManagementSystem.WebApi/Controllers/EmployeeController.cs
@@ -1,66 +1,68 @@
-﻿using EmployeeManagementSystem.Model.Models.Employee;
+﻿namespace EmployeeManagementSystem.WebApi.Controllers;
 
-namespace EmployeeManagementSystem.WebApi.Controllers
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class EmployeeController : ControllerBase
 {
-    [Authorize]
-    [Route("api/[controller]")]
-    [ApiController]
-    public class EmployeeController : ControllerBase
+    private readonly IEmployeeService employeeService;
+    private readonly IDapperService dapperService;
+
+    public EmployeeController(IEmployeeService employeeService, IDapperService dapperService)
     {
-        private readonly IEmployeeService employeeService;
-        private readonly IDapperService dapperService;
+        this.employeeService = employeeService;
+        this.dapperService = dapperService;
+    }
 
-        public EmployeeController(IEmployeeService employeeService, IDapperService dapperService)
-        {
-            this.employeeService = employeeService;
-            this.dapperService = dapperService;
-        }
+    [HttpGet("employeeList")]
+    public async Task<ActionResult<BaseResponseModel>> GetEmployeeList()
+    {
+        var employees = await employeeService.GetEmployeeListAsync();
+        if (employees is null)
+            return BadRequest(new BaseResponseModel
+                { Data = null, IsSuccess = true, Message = "Employee Not Found" });
+        return Ok(new BaseResponseModel { Data = employees, IsSuccess = true, Message = "Employee List" });
+    }
 
-        [HttpGet("employeeList")]
-        public async Task<ActionResult<BaseResponseModel>> GetEmployeeList()
-        {
-            var employees = await employeeService.GetEmployeeListAsync();
-            if (employees is null)
-                return BadRequest(new BaseResponseModel { Data = null, IsSuccess = true, Message = "Employee Not Found" });
-            return Ok(new BaseResponseModel { Data = employees, IsSuccess = true, Message = "Employee List" });
-        }
+    [HttpPost("createEmployee")]
+    public async Task<ActionResult<BaseResponseModel>> CreateEmployee(EmployeeRequestModel employeeModel)
+    {
+        var res = await employeeService.CreateEmployee(employeeModel);
+        if (res > 0)
+            return Ok(new BaseResponseModel
+                { Data = employeeModel, IsSuccess = true, Message = "Employee Creation Successful" });
+        return BadRequest(new BaseResponseModel
+            { Data = null, IsSuccess = false, Message = "Employee Creation Failed" });
+    }
 
-        [HttpPost("createEmployee")]
-        public async Task<ActionResult<BaseResponseModel>> CreateEmployee(EmployeeRequestModel employeeModel)
-        {
-            var res = await employeeService.CreateEmployee(employeeModel);
-            if (res > 0) return Ok(new BaseResponseModel { Data = employeeModel, IsSuccess = true, Message = "Employee Creation Successful" });
-            return BadRequest(new BaseResponseModel { Data = null, IsSuccess = false, Message = "Employee Creation Failed" });
-        }
+    [HttpGet("getEmployee/{id}")]
+    public async Task<ActionResult<BaseResponseModel>> EditEmployee(int id)
+    {
+        var res = await employeeService.GetEmployeeById(id);
+        if (res is null)
+            return BadRequest(new BaseResponseModel
+                { Data = null, IsSuccess = false, Message = "Employee Not Found" });
+        return Ok(new BaseResponseModel { Data = res, IsSuccess = true, Message = "Employee Found" });
+    }
 
-        [HttpGet("getEmployee/{id}")]
-        public async Task<ActionResult<BaseResponseModel>> EditEmployee(int id)
-        {
-            var res = await employeeService.GetEmployeeById(id);
-            if (res is null)
-                return BadRequest(new BaseResponseModel { Data = null, IsSuccess = false, Message = "Employee Not Found" });
-            return Ok(new BaseResponseModel { Data = res, IsSuccess = true, Message = "Employee Found" });
-        }
+    [HttpPatch("updateEmployee/{id}")]
+    public async Task<ActionResult<BaseResponseModel>> UpdateEmployee(int id, EmployeeModel employee)
+    {
+        var res = await employeeService.UpdateEmployee(id, employee);
+        if (res is 0)
+            return BadRequest(new BaseResponseModel
+                { Data = employee, IsSuccess = false, Message = "Employee Updating Failed" });
+        return Ok(new BaseResponseModel
+            { Message = "Employee Updating Successful", IsSuccess = true, Data = employee });
+    }
 
-        [HttpPatch("updateEmployee/{id}")]
-        public async Task<ActionResult<BaseResponseModel>> UpdateEmployee( int id, EmployeeModel employee)
-        {
-            var res = await employeeService.UpdateEmployee(id, employee);
-            if (res is 0)
-                return BadRequest(new BaseResponseModel { Data = employee, IsSuccess = false, Message = "Employee Updating Failed" });
-            return Ok(new BaseResponseModel { Message = "Employee Updating Successful", IsSuccess = true, Data = employee });
-        }
-
-        [HttpDelete("deleteEmployee/{id}")]
-        public async Task<ActionResult<BaseResponseModel>> DeleteEmployee( int id)
-        {
-            var res = await employeeService.DeleteEmployee(id);
-            if (res is 0)
-                return BadRequest(new BaseResponseModel { Data = null, IsSuccess = false, Message = "Employee Delete Failed" });
-            return Ok(new BaseResponseModel { IsSuccess = true, Message = "Employee Delete Successful" });
-        }
-
-
+    [HttpDelete("deleteEmployee/{id}")]
+    public async Task<ActionResult<BaseResponseModel>> DeleteEmployee(int id)
+    {
+        var res = await employeeService.DeleteEmployee(id);
+        if (res is 0)
+            return BadRequest(new BaseResponseModel
+                { Data = null, IsSuccess = false, Message = "Employee Delete Failed" });
+        return Ok(new BaseResponseModel { IsSuccess = true, Message = "Employee Delete Successful" });
     }
 }
-
