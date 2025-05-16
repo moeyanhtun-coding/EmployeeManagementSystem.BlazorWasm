5d4593e1b320637f2f4ef3017c6abecba6216c1a
moeyanhtun-coding <moeyan227970@gmail.com>
2025-05-16 16:17:19 +0630

    [Refactor] Add Global Using

--- EmployeeManagementSystem.Mapper/GlobalUsing.cs
+++
@@ -1,5 +0,0 @@
-﻿global using BCrypt.Net;
-global using EmployeeManagementSystem.Model.Entities;
-global using EmployeeManagementSystem.Model.Models.Auth;
-global using EmployeeManagementSystem.Model.Models.Employee;
-global using Microsoft.Identity.Client;
