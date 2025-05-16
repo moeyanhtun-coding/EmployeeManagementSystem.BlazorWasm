e07a409df07957105ec79f96e5bc4ebc2254a4e2
moeyanhtun-coding <moeyan227970@gmail.com>
2025-05-16 16:17:28 +0630

    [Refactor] Add Global Using

---
+++ EmployeeManagementSystem.Mapper/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using EmployeeManagementSystem.Model.Models.Attendance;
+global using EmployeeManagementSystem.Model.Entities;
+global using EmployeeManagementSystem.Model.Models.Auth;
+global using EmployeeManagementSystem.Model.Models.Employee;
