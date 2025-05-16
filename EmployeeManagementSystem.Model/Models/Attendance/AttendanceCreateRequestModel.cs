a1a4b65241b8dcbcaf9ef7bec1a4a29a03477307
moeyanhtun-coding <moeyan227970@gmail.com>
2025-05-16 16:17:00 +0630

    [Refactor] Add Global Using

--- EmployeeManagementSystem.Model/Models/Attendance/AttendanceCreateRequestModel.cs
+++ EmployeeManagementSystem.Model/Models/Attendance/AttendanceCreateRequestModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EmployeeManagementSystem.Model.Models.Attendance
+﻿namespace EmployeeManagementSystem.Model.Models.Attendance
 {
     public class AttendanceCreateRequestModel
     {
