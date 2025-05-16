67c8c355e52126cc49765c5b921de21132bd77bf
moeyanhtun-coding <moeyan227970@gmail.com>
2025-05-16 15:14:17 +0630

    -

--- EmployeeManagementSystem.Wasm/Pages/Employee/EmployeeListPage.razor.cs
+++ EmployeeManagementSystem.Wasm/Pages/Employee/EmployeeListPage.razor.cs
@@ -1,16 +1,22 @@
-﻿using Microsoft.AspNetCore.WebUtilities;
+﻿using EmployeeManagementSystem.Model.Models.Employee;
+using EmployeeManagementSystem.Wasm.Services;
+using EmployeeManagementSystem.Wasm.Services.Endpoints;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace EmployeeManagementSystem.Wasm.Pages.Employee
 {
     public partial class EmployeeListPage
     {
+        private int pageCount;
+        private int pageNo = 1;
+        private int pageSize = 10;
         private string sortName;
         private bool isShow = false;
         private string? AlertMessage;
         private string AlertIcon;
         private string AlertColor;
         private List<EmployeeModel> employeeModels;
-        private List<EmployeeModel> employeeList;
+        private EmployeeListResponseModel employeeList;
         private BaseResponseModel? baseResponseModel = new();
         private int DeleteId;
         private AppModal Modal;
@@ -19,7 +25,7 @@ namespace EmployeeManagementSystem.Wasm.Pages.Employee
 
         protected override async Task OnInitializedAsync()
         {
-            await GetEmployees();
+            await PageChanged(pageNo);
         }
         protected async override Task OnAfterRenderAsync(bool firstRender)
         {
@@ -57,23 +63,24 @@ namespace EmployeeManagementSystem.Wasm.Pages.Employee
             sortName = sort;
             if (sort == "Ascending")
             {
-                employeeModels = employeeList;
+                employeeModels = employeeList.DataList;
             }else if (sort == "Descending")
             {
-                employeeModels = employeeList.OrderByDescending(x => x.EmployeeCode).ToList();
+                employeeModels = employeeList.DataList.OrderByDescending(x => x.EmployeeCode).ToList();
             }
         }
         public async Task GetEmployees()
         {
             await devCode.SetAuthorizeHeader();
-            var response = await httpClient.GetAsync("api/Employee/employeeList");
+            var response = await httpClient.GetAsync(EmployeeEndpoints.GetListPagination(pageNo, pageSize));
             if (response.IsSuccessStatusCode)
             {
                 var jsonResponse = await response.Content.ReadAsStringAsync();
                 baseResponseModel = JsonConvert.DeserializeObject<BaseResponseModel>(jsonResponse)!;
                 if (baseResponseModel!.IsSuccess)
                 {
-                    employeeList = JsonConvert.DeserializeObject<List<EmployeeModel>>(baseResponseModel.Data!.ToString()!)!;
+                    employeeList = JsonConvert.DeserializeObject<EmployeeListResponseModel>(baseResponseModel.Data.ToString())!;
+                    pageCount = employeeList.PageSetting.PageCount;
                     EmployeeListSorting("Ascending");
                 }
             }
@@ -87,7 +94,7 @@ namespace EmployeeManagementSystem.Wasm.Pages.Employee
         private async Task Delete()
         {
             await devCode.SetAuthorizeHeader();
-            var res = await httpClient.DeleteAsync($"api/Employee/deleteEmployee/{DeleteId}");
+            var res = await httpClient.DeleteAsync( EmployeeEndpoints.Delete(DeleteId));
             if (res.IsSuccessStatusCode)
             {
                 var jsonStr = await res.Content.ReadAsStringAsync();
@@ -109,6 +116,12 @@ namespace EmployeeManagementSystem.Wasm.Pages.Employee
                 var error = await res.Content.ReadAsStringAsync();
                 Console.WriteLine("Error occurred: " + error);
             }
+        }
+
+        public async Task PageChanged(int i)
+        {
+            pageNo = i;
+            await GetEmployees();
         }
         public void AlertFunction(string alertMessage, string icon, string alertColor)
         {
